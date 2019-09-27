762592eaa64d9bfb8977159dce4b4a4adb483a31
Miguel Lama <migue.lama@yahoo.com>
2019-09-27 11:59:50 -0500

    Cmake

---
+++ Codigo/Source.cpp
@@ -0,0 +1,7 @@
+#include <Headers/Head.h>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
---
+++ Matrices/Matrices.cpp
@@ -0,0 +1,12 @@
+﻿// Matrices.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Matrices.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola CMake." << endl;
+	return 0;
+}
