ff622574154637bbbd2677d2435c75bd2b8da83c
Miguel Lama <migue.lama@yahoo.com>
2019-09-27 12:50:18 -0500

    Rotacion de matrices basica

--- Matrices/Matrices.h
+++ Matrices/Matrices.h
@@ -5,8 +5,8 @@
 
 
 using numero = int;
-using puntero = numero*;
-using doble_puntero = puntero*;
+using puntero = numero *;
+using doble_puntero = puntero *;
 using palabra = std::string;
 using funcion = doble_puntero;
 using funcion_vacia = void;
@@ -15,12 +15,12 @@ class variables
 {
 	funcion crear_arreglo(numero filas, numero columnas)
 	{
-		 doble_puntero lista_dinamica = new puntero[filas];
-		 for (numero i = 0; i < filas; i++)
-		 {
-			 lista_dinamica[i] = new int[columnas];
-		 }
-		 return lista_dinamica;
+		doble_puntero lista_dinamica = new puntero[filas];
+		for (numero i = 0; i < filas; i++)
+		{
+			lista_dinamica[i] = new int[columnas];
+		}
+		return lista_dinamica;
 	}
 	funcion_vacia swap(numero& x, numero& y)
 	{
@@ -32,16 +32,16 @@ class variables
 	{
 		for (numero i; i < x_tamano; i++)
 		{
-			swap(*(x+i), *(y+i));
+			swap(*(x + i), *(y + i));
 		}
 	}
 	funcion_vacia swap_filas_matriz(doble_puntero x, numero filas_tamano, numero columnas_tamano)
 	{
-		for (numero i = filas_tamano; i > 0; i-=1)
+		for (numero i = filas_tamano; i > 0; i -= 1)
 		{
-			swap_columnas(*(x + i), *(x + i - 1),columnas_tamano);
+			swap_columnas(*(x + i), *(x + i - 1), columnas_tamano);
 		}
 	}
 
-	
+
 };
